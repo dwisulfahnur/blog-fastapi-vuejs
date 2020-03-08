7fed123cd77cce9e93726228ce58b127e6097f45
Dwi Sulfahnur <hello@dwisulfahnur.com>
2020-03-08 14:55:59 +0700

    conf: add some variable to the config file

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,14 +1,23 @@
-from starlette.config import Config
-from starlette.datastructures import Secret, CommaSeparatedStrings
+from passlib.context import CryptContext
+from starlette.config import Config, environ
+from starlette.datastructures import CommaSeparatedStrings
 
 config = Config(".env")
 
-SECRET_KEY = config('SECRET_KEY', cast=Secret)
+BASE_PATH_API = config('BASE_PATH_API')
+
+SECRET_KEY = config('SECRET_KEY')
+ALGORITHM = "HS256"
+PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=CommaSeparatedStrings)
 
 DEBUG = config('DEBUG', cast=bool, default=False)
 
+ACCESS_TOKEN_EXPIRES = config('ACCESS_TOKEN_EXPIRES', default=24 * 7)
 MONGODB_NAME = config("MONGODB_NAME")
 MONGODB_URI = config("MONGODB_URI")
 
 database_name = MONGODB_NAME
+if environ.get('TESTING') == 'TRUE':
+    database_name = f'test-{database_name}'
