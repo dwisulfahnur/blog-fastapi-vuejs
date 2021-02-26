a35af1b02672429dbbef5be786acbc19a87a4047
Dwi Sulfahnur <hello@dwisulfahnur.com>
2021-02-26 21:21:20 +0700

    feat(user): add loading transition in every request to API

--- frontend/src/components/posts/MyPosts.vue
+++ frontend/src/components/posts/MyPosts.vue
@@ -1,26 +1,43 @@
 <template>
     <div class="col-md-8">
-        <h3>My Posts</h3>
-        <table class="table table-hover">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Title</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
+        <div class="card">
+            <div class="card-body" v-if="loading">
+                <div class="spinner-grow text-primary" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+            <div class="card-body" v-else-if="posts.length > 0">
+                <h3 class="card-title">
+                    My Posts
+                </h3>
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
 
-            <tbody>
-            <tr :key="post.id" v-for="(post, index) in posts">
-                <th style="width: 10px">{{ index + 1 }}</th>
-                <td>{{ post.title }}</td>
-                <td>
-                    <a class="btn btn-outline-primary btn-sm mr-2">Edit</a>
-                    <a class="btn btn-outline-danger btn-sm">Delete</a>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+                    <tbody>
+                    <tr :key="post.id" v-for="(post, index) in posts">
+                        <th style="width: 10px">{{ index + 1 }}</th>
+                        <td>{{ post.title }}</td>
+                        <td>
+                            <router-link :to="{name:'edit-post',params: {id: post.id}}"
+                                         class="btn btn-outline-primary btn-sm mr-2">Edit
+                            </router-link>
+                            <a @click="deleteItem(post.id)" class="btn btn-outline-danger btn-sm">Delete</a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-body text-center" v-else>
+                <h3>You don't have any post</h3>
+                <router-link :to="{name:'new-post'}" class="btn btn-outline-primary">Create a new one</router-link>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -29,13 +46,28 @@
         name: "MyPosts",
         data: function () {
             return {
+                loading: false,
                 posts: []
             }
         },
         mounted() {
-            this.axios.get('/posts/me').then(response => {
-                this.posts = response.data
-            })
+            this.loadPosts()
+        },
+        methods: {
+            loadPosts() {
+                this.loading = true
+                this.axios.get('/posts/me').then(response => {
+                    this.posts = response.data
+                    this.loading = false
+                })
+            },
+            deleteItem(id) {
+                if (confirm('Are you sure to delete the post?')) {
+                    this.axios.delete(`/posts/${id}`).then(() => {
+                        this.loadPosts()
+                    })
+                }
+            }
         }
     }
 </script>
