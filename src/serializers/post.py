e68dab45dd1ac7885a857a51070b5cc1a64978bd
Dwi Sulfahnur <hello@dwisulfahnur.com>
2021-02-26 18:33:31 +0700

    feat(post): add validation process to the post serializer

--- src/serializers/post.py
+++ src/serializers/post.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 from .comment import CommentSerializer
 
@@ -20,6 +20,18 @@ class PostSerializer(BaseModel):
 class PostInSerializer(BaseModel):
     title: str
     content: str
+
+    @validator('title', 'content')
+    def validate_empty_data(cls, v):
+        if not v:
+            raise ValueError('This field is required')
+        return v
+
+    @validator('title')
+    def validate_title(cls,v):
+        if len(v) < 5:
+            raise ValueError('Minimum length of title is 5 chars')
+        return v
 
 
 class PostInPatchSerializer(BaseModel):
