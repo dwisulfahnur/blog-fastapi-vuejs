b4cf569d7b62c75b679086e2e9736dae4717cf2b
Dwi Sulfahnur <hello@dwisulfahnur.com>
2021-02-26 21:30:06 +0700

    feat(user): set full_name as required field on the user serializer

--- src/serializers/user.py
+++ src/serializers/user.py
@@ -17,12 +17,20 @@ class UserSerializer(BaseModel):
 
 
 class UserInSerializer(BaseModel):
-    full_name: Optional[str]
+    full_name: str
     email: EmailStr
     password: str
 
+    @validator('full_name')
+    def validate_full_name(cls, v):
+        if not v:
+            raise ValueError('Full Name is required')
+        return v
+
     @validator('password')
     def validate_password(cls, v):
+        if not v:
+            raise ValueError('Password is required to create a new account')
         if v and len(v) < 8:
             raise ValueError('Password must be more than 8 characters')
         return v
