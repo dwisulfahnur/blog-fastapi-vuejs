a0fecc30a92cfa0c982336dd594aaf7113bfa426
Dwi Sulfahnur <hello@dwisulfahnur.com>
2021-02-26 21:29:42 +0700

    feat(post): update validator for the post serializer

--- src/serializers/post.py
+++ src/serializers/post.py
@@ -12,6 +12,7 @@ class PostSerializer(BaseModel):
     content: str
     comments: List[CommentSerializer] = []
 
+    author_name: Optional[str]
     created_by: str
     created_at: datetime
     updated_at: datetime
@@ -21,16 +22,20 @@ class PostInSerializer(BaseModel):
     title: str
     content: str
 
-    @validator('title', 'content')
-    def validate_empty_data(cls, v):
+    @validator('title')
+    def validate_title(cls, v):
         if not v:
             raise ValueError('This field is required')
-        return v
-
-    @validator('title')
-    def validate_title(cls,v):
         if len(v) < 5:
             raise ValueError('Minimum length of title is 5 chars')
+        return v
+
+    @validator('content')
+    def validate_content(cls, v):
+        if not v:
+            raise ValueError('This field is required')
+        if len(v) < 20:
+            raise ValueError('Minimum length of the body of content is 20 chars')
         return v
 
 
