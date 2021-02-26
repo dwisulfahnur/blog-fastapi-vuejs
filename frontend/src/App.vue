97629cafceceb7b4dbc8d903a13570684d0c368c
Dwi Sulfahnur <hello@dwisulfahnur.com>
2021-02-26 17:50:06 +0700

    feat(nav): using new component for nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,37 +1,33 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-expand-sm bg-light">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link :to="{name: 'user-create'}" class="nav-link">New User</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{name: 'user-list'}" class="nav-link">User List</router-link>
-        </li>
-      </ul>
-    </nav>
+    <div id="app">
+        <Nav/>
 
-    <transition name="fade">
-      <div class="gap">
-        <div class="container mt-4">
-          <router-view></router-view>
-        </div>
-      </div>
-    </transition>
-  </div>
+        <transition name="fade">
+            <div class="gap">
+                <div class="container mt-4 d-flex justify-content-center">
+                    <router-view></router-view>
+                </div>
+            </div>
+        </transition>
+    </div>
 </template>
 
 <script>
-export default {};
+    import Nav from "./components/Nav";
+
+    export default {
+        components: {Nav}
+    };
 </script>
 
 <style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter,
-.fade-leave-active {
-  opacity: 0;
-}
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.5s;
+    }
+
+    .fade-enter,
+    .fade-leave-active {
+        opacity: 0;
+    }
 </style>
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,59 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+            <button aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation"
+                    class="navbar-toggler"
+                    data-target="#navbarNav" data-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <a class="navbar-brand" href="/">Blog</a>
+                <ul class="navbar-nav mr-auto"></ul>
+                <ul class="navbar-nav my-2 my-lg-0" v-if="loggedIn()">
+                    <li class="nav-item">
+                        {{userEmail}}
+                    </li>
+                    <li class="nav-item ml-2">
+                        <a href="javascript:void(0)" v-on:click="logOut">Log Out</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav my-2 my-lg-0" v-else>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'register'}" class="nav-link">Register</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{name: 'login'}" class="nav-link">Login</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Nav",
+        computed: {
+            userEmail() {
+                return localStorage.getItem('email')
+            }
+        },
+        methods: {
+            loggedIn() {
+                console.log(localStorage.getItem('loggedIn'))
+                return localStorage.getItem('loggedIn')
+            },
+            logOut() {
+                localStorage.removeItem('actoken')
+                localStorage.removeItem('loggedIn')
+                localStorage.removeItem('email')
+                this.$router.replace({name: 'login'})
+                window.location.reload()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
