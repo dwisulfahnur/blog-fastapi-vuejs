ef77e144dcae67e2d50ec3d7429d834c0f6fbb27
Dwi Sulfahnur <hello@dwisulfahnur.com>
2021-02-26 17:51:36 +0700

    feat: register the new components to the routerg

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,52 +5,74 @@ import axios from 'axios'
 import NProgress from 'nprogress'
 
 import App from './App.vue'
-import CreateUser from './components/users/CreateUser'
+import CreateUser from './components/users/Register'
 import EditUser from './components/users/EditUser'
-import ListUser from './components/users/ListUser'
-
+import Login from './components/auth/Login'
 import 'bootstrap/dist/css/bootstrap.css'
+import MyPosts from "./components/posts/MyPosts";
+import PublicPosts from "./components/posts/PublicPosts";
+// import ListUser from './components/users/ListUser'
 
 
 Vue.use(VueRouter)
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios.create({
+    baseURL: 'http://localhost:8000/api',
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem('actoken')}`
+    }
+}))
 
 Vue.config.productionTip = false
 
 
 const routes = [
-  {
-    name: 'user-list',
-    path: '/users/',
-    component: ListUser
-  },
-  {
-    name: 'user-create',
-    path: '/users/create',
-    component: CreateUser
-  },
-  {
-    name: 'user-edit',
-    path: '/users/edit',
-    component: EditUser
-  }
+    // {
+    //   name: 'user-list',
+    //   path: '/users/',
+    //   component: ListUser
+    // },
+    {
+        name: 'Home',
+        path: '/',
+        component: PublicPosts
+    },
+    {
+        name: 'login',
+        path: '/login',
+        component: Login
+    },
+    {
+        name: 'register',
+        path: '/register',
+        component: CreateUser
+    },
+    {
+        name: 'user-edit',
+        path: '/users/edit',
+        component: EditUser
+    },
+    {
+        name: 'my-posts',
+        path: '/post/me',
+        component: MyPosts,
+    }
 ]
 
 
-const router = new VueRouter({ mode: 'history', routes: routes })
+const router = new VueRouter({mode: 'history', routes: routes})
 
 router.beforeResolve((to, from, next) => {
-  if (to.name) {
-    NProgress.start()
-  }
-  next()
+    if (to.name) {
+        NProgress.start()
+    }
+    next()
 })
 
 router.afterEach(() => {
-  NProgress.done()
+    NProgress.done()
 })
 
 new Vue({
-  render: h => h(App),
-  router
+    render: h => h(App),
+    router
 }).$mount('#app')
